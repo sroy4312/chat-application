0d358b9f80dce354e7092e4701d7f487e7f53976
sroy4312 <145863894+sroy4312@users.noreply.github.com>
2023-10-04 00:44:31 +0530

    Added index.js and UserTextInput.jsx files

---
+++ components/UserTextInput.jsx
@@ -0,0 +1,35 @@
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useLayoutEffect } from 'react';
+import { MaterialIcons, Entypo } from '@expo/vector-icons';
+
+const UserTextInput = ({placeholder, isPass, setStateValue}) => {
+  const [value, setValue] = useState("");
+  const [showPass, setShowPass] = useState(true);
+  const [icon, setIcon] = useState(null);
+  const handleTextChanged = (text) => {
+    setValue(text);
+    setStateValue(value);
+  }
+  useLayoutEffect(() => {
+    switch(placeholder) {
+        case "Full Name" : return setIcon("person")
+        case "Email": return setIcon("email")
+        case "Password": return setIcon("lock")
+    }
+  }, [])
+  return (
+    <View className={`border rounded-2xl px-4 py-6 flex-row items-center justify-between space-x-4 my-2 border-gray-200`}>
+      <MaterialIcons name={icon} size={24} color={"#6C6D83"} />
+      <TextInput className="flex-1 text-base text-primaryText font-semibold -mt-1" placeholder={placeholder} value={value} onChangeText={handleTextChanged} secureTextEntry={isPass && showPass} autoCapitalize='none' />
+      {
+        isPass && (
+            <TouchableOpacity onPress={() => setShowPass(!showPass)}>
+                <Entypo name={`${showPass ? "eye" : "eye-with-line"}`} size={24} color={"#6C6D83"} />
+            </TouchableOpacity>
+        )
+      }
+    </View>
+  )
+}
+
+export default UserTextInput;
