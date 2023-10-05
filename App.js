05bf67dd66969e0507b075aa27296ea5826ccf42
sroy4312 <145863894+sroy4312@users.noreply.github.com>
2023-10-06 00:06:26 +0530

    Modified the App.js file

--- App.js
+++ App.js
@@ -2,17 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen, SignupScreen } from './screens/index';
+import { Provider } from 'react-redux';
+import { LoginScreen, SignupScreen, SplashScreen, HomeScreen } from './screens/index';
+import Store from './context/store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="SignupScreen" component={SignupScreen} />
-      </Stack.Navigator>
+      <Provider store={Store}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
